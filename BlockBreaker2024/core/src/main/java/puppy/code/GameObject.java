21773d3b32aaae2fec189554d655fd4a3305b60c
Javier <xavier.montoya.s@mail.pucv.cl>
2024-10-14 20:37:55 -0300

    GameObject implementado

---
+++ BlockBreaker2024/core/src/main/java/puppy/code/GameObject.java
@@ -0,0 +1,52 @@
+package puppy.code;
+
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public abstract class GameObject {
+    protected float posX;
+    protected float posY;
+    protected float ancho;
+    protected float alto;
+    protected Texture textura;
+    protected String sfx;
+
+    public void setPosicion(float x, float y) {
+        posX = x;
+        posY = y;
+    }
+
+    public void setDimensiones(float ancho, float alto) {
+        this.ancho = ancho;
+        this.alto = alto;
+    }
+
+    public float getPosX() {
+        return posX;
+    }
+
+    public float getPosY() {
+        return posY;
+    }
+
+    public float getAncho() {
+        return ancho;
+    }
+
+    public float getAlto() {
+        return alto;
+    }
+
+    public void setTextura(String nombreTextura) {
+        GestorTexturas gestorTexturas = GestorTexturas.getInstance();
+        textura = gestorTexturas.getTextura(nombreTextura);
+    }
+
+    public void setSfx(String nombreSfx) {
+        sfx = nombreSfx;
+    }
+
+    public abstract void draw(SpriteBatch batch);
+    public abstract void render(SpriteBatch batch);
+}
