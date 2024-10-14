e0e8ed5a1fbe47c052f1b9f92d6434413d0153ae
Javier <xavier.montoya.s@mail.pucv.cl>
2024-10-14 20:36:43 -0300

    PingBall implementado

--- BlockBreaker2024/core/src/main/java/puppy/code/PingBall.java
+++ BlockBreaker2024/core/src/main/java/puppy/code/PingBall.java
@@ -1,85 +1,70 @@
 package puppy.code;
 
-
-
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class PingBall {
-	    private int x;
-	    private int y;
-	    private int size;
-	    private int xSpeed;
-	    private int ySpeed;
-	    private Color color = Color.WHITE;
-	    private boolean estaQuieto;
-	    
-	    public PingBall(int x, int y, int size, int xSpeed, int ySpeed, boolean iniciaQuieto) {
-	        this.x = x;
-	        this.y = y;
-	        this.size = size;
-	        this.xSpeed = xSpeed;
-	        this.ySpeed = ySpeed;
-	        estaQuieto = iniciaQuieto;
-	    }
-	    
-	    public boolean estaQuieto() {
-	    	return estaQuieto;
-	    }
-	    public void setEstaQuieto(boolean bb) {
-	    	estaQuieto=bb;
-	    }
-	    public void setXY(int x, int y) {
-	    	this.x = x;
-	        this.y = y;
-	    }
-	    public int getY() {return y;}
-	    
-	    public void draw(ShapeRenderer shape){
-	        shape.setColor(color);
-	        shape.circle(x, y, size);
-	    }
-	    
-	    public void update() {
-	    	if (estaQuieto) return;
-	        x += xSpeed;
-	        y += ySpeed;
-	        if (x-size < 0 || x+size > Gdx.graphics.getWidth()) {
-	            xSpeed = -xSpeed;
-	        }
-	        if (y+size > Gdx.graphics.getHeight()) {
-	            ySpeed = -ySpeed;
-	        }
-	    }
-	    
-	    public void checkCollision(Paddle paddle) {
-	        if(collidesWith(paddle)){
-	            color = Color.GREEN;
-	            ySpeed = -ySpeed;
-	        }
-	        else{
-	            color = Color.WHITE;
-	        }
-	    }
-	    private boolean collidesWith(Paddle pp) {
+public class PingBall extends GameObject {
+	private float xSpeed;
+	private float ySpeed;
+	private boolean estaQuieto;
 
-	    	boolean intersectaX = (pp.getX() + pp.getWidth() >= x-size) && (pp.getX() <= x+size);
-	        boolean intersectaY = (pp.getY() + pp.getHeight() >= y-size) && (pp.getY() <= y+size);		
-	    	return intersectaX && intersectaY;
-	    }
-	    
-	    public void checkCollision(Block block) {
-	        if(collidesWith(block)){
-	            ySpeed = - ySpeed;
-	            block.destroyed = true;
-	        }
-	    }
-	    private boolean collidesWith(Block bb) {
-
-	    	boolean intersectaX = (bb.x + bb.width >= x-size) && (bb.x <= x+size);
-	        boolean intersectaY = (bb.y + bb.height >= y-size) && (bb.y <= y+size);		
-	    	return intersectaX && intersectaY;
-	    }
-	    
+	public PingBall(float x, float y, float size, float xSpeed, float ySpeed, boolean iniciaQuieto, String nombreTextura) {
+		setPosicion(x, y);
+		setDimensiones(size, size);
+		setTextura(nombreTextura);
+		this.xSpeed = xSpeed;
+		this.ySpeed = ySpeed;
+		estaQuieto = iniciaQuieto;
 	}
+
+	public boolean estaQuieto() {
+		return estaQuieto;
+	}
+
+	public void setEstaQuieto(boolean bb) {
+		estaQuieto=bb;
+	}
+
+	public void setxSpeed(float xSpeed) {
+		this.xSpeed = xSpeed;
+	}
+
+	public float getxSpeed() {
+		return xSpeed;
+	}
+
+	public void setySpeed(float ySpeed) {
+		this.ySpeed = ySpeed;
+	}
+
+	public float getySpeed() {
+		return ySpeed;
+	}
+
+	public void multiplicarVelocidad(float factor) {
+		xSpeed *= factor;
+		ySpeed *= factor;
+	}
+
+	@Override
+	public void draw(SpriteBatch batch) {
+		batch.draw(textura, posX - ancho, posY - alto, ancho * 2, alto * 2);
+	}
+
+	@Override
+	public void render(SpriteBatch batch) {
+		// TODO Auto-generated method stub
+	}
+
+	public void update() {
+		if (estaQuieto) return;
+		posX += xSpeed;
+		posY += ySpeed;
+		if (posX - ancho < 0 || posX + ancho > Gdx.graphics.getWidth()) {
+			xSpeed = -xSpeed;
+		}
+		if (posY + alto > Gdx.graphics.getHeight()) {
+			ySpeed = -ySpeed;
+		}
+	}
+}
